b91807bc75687e153f611403180ffc034913d67a
EMUJAHI <emujahi@alumno.upv.es@ldsic-vdi16.upvnet.upv.es>
2026-01-03 18:54:33 +0100

    Añadida implementación de la clase ListLinked

--- ListLinked.h
+++ ListLinked.h
@@ -1,6 +1,7 @@
 #include <ostream>
 #include "List.h"
 #include "Node.h"
+#include <stdexcept>
 
 template <typename T>
 class ListLinked : public List<T> {
@@ -21,7 +22,7 @@ public:
         }
     }
 
-    T operator[](int pos) override {
+    T operator[](int pos){
         if (pos < 0 || pos >= n) {
             throw std::out_of_range("Posición fuera de rango");
         }
@@ -40,5 +41,88 @@ public:
         }
         return out;
     }
-};
+    void insert(int pos, T e) override {
+        if (pos < 0 || pos > n) {
+            throw std::out_of_range("posicion invalida");
+        }
 
+        if (pos == 0) {
+            first = new Node<T>(e, first);
+        } else {
+            Node<T>* prev = first;
+            for (int i = 0; i < pos - 1; ++i) {
+                prev = prev->next;
+            }
+            prev->next = new Node<T>(e, prev->next);
+        }
+        ++n;
+    }
+
+    void append(T e) override {
+        insert(n, e);
+    }
+
+    void prepend(T e) override {
+        insert(0, e);
+    }
+
+    T remove(int pos) override {
+        if (pos < 0 || pos >= n) {
+            throw std::out_of_range("posicion invalida");
+        }
+
+        Node<T>* toDelete;
+        T value;
+
+        if (pos == 0) {
+            toDelete = first;
+            value = first->data;
+            first = first->next;
+        } else {
+            Node<T>* prev = first;
+            for (int i = 0; i < pos - 1; ++i) {
+                prev = prev->next;
+            }
+            toDelete = prev->next;
+            value = toDelete->data;
+            prev->next = toDelete->next;
+        }
+
+        delete toDelete;
+        --n;
+        return value;
+    }
+
+    T get(int pos) const override {
+        if (pos < 0 || pos >= n) {
+            throw std::out_of_range("posicion invalida");
+        }
+
+        Node<T>* aux = first;
+        for (int i = 0; i < pos; ++i) {
+            aux = aux->next;
+        }
+        return aux->data;
+    }
+
+    int search(T e) const override {
+        Node<T>* aux = first;
+        int i = 0;
+        while (aux != nullptr) {
+            if (aux->data == e) {
+                return i;
+            }
+            aux = aux->next;
+            ++i;
+        }
+        return -1;
+    }
+
+    bool empty() const override {
+        return n == 0;
+    }
+
+    int size() const override {
+        return n;
+    }
+};
